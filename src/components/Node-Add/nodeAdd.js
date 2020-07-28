4b75eada36a35944d38de7f7a8826a5f044b1c6d
wenjuanW-Ds <280690@supinfo.com>
2020-07-28 10:39:51 +0200

    commit

--- src/components/Node-Add/nodeAdd.js
+++ src/components/Node-Add/nodeAdd.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {Button, Toolbar,TextField} from "@material-ui/core";
 import PropTypes from 'prop-types'
 
-import { green, purple } from '@material-ui/core/colors';
+
 
 export default class NodeAdd extends React.Component{
 
